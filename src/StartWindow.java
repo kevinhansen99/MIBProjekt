afc8bc7701c162cef5edd1f378f42553f74ad868
Timo <Work@DESKTOP-4IKD19O.lan>
2020-12-22 22:54:24 +0100

    test

--- src/StartWindow.java
+++ src/StartWindow.java
@@ -25,7 +25,6 @@ public class StartWindow extends javax.swing.JFrame {
         initComponents();
         this.idb = idb;
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -144,82 +143,9 @@ public class StartWindow extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void buttonLoginAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonLoginAgentActionPerformed
-        String usernameText = IdFieldAgent.getText();
-        String passwordText = passwordAgent.getText();
-        
-        if(Validation.textEmptyValidation(IdFieldAgent) && (Validation.IsInt(IdFieldAgent))){
-            
-             try {
-                //Hämtar in användarnamn (agent_ID) från databasen.
-                String usernameDataBase = idb.fetchSingle("SELECT AGENT_ID from AGENT where AGENT_ID = " + usernameText);
-                //Hämtar in lösenord från databasen.
-
-                String passwordDataBase = idb.fetchSingle("SELECT LOSENORD from AGENT where AGENT_ID = " + usernameText);
-                //Hämtar in om användare är admin från databasen.
-                String admin = idb.fetchSingle("SELECT ADMINISTRATOR from AGENT where AGENT_ID = " + usernameText);
-                //Kollar om användarnamn och lösenord från databasen stämmer överens med det användare matat in SAMT om användare är admin.
-                if (usernameText.equals(usernameDataBase) && passwordText.equals(passwordDataBase) && admin.equals("J")) {
-                    //Sparar användarens ID till ett fält som sedan kan användas av andra klasser för att hitta "ID" på vem som är inloggad.
-                    userId = usernameDataBase;
-                    setVisible(false);
-                    //Öppnar ny admin ruta.
-                    AdministratorWindow adminWindow = new AdministratorWindow(idb);
-                    adminWindow.setVisible(true);
-                } // Om användare skriver in rätt användarnamn OCH rätt lösenord.
-                else if (usernameText.equals(usernameDataBase) && passwordText.equals(passwordDataBase)) {
-                    //Sparar användarens ID till ett fält som sedan kan användas av andra klasser för att hitta "ID" på vem som är inloggad.
-                    userId = usernameDataBase;
-                    setVisible(false);
-                    //Öppnar ny agent (icke-admin) ruta.
-                    AgentWindow agentWindow = new AgentWindow(idb);
-                    agentWindow.setVisible(true);
-
-                } else {
-                    //Om fel användarnamn eller lösenord.
-                    JOptionPane.showMessageDialog(null, "Fel lösenord eller användarnamn!");
-                }
-            } catch (InfException exc) {
-                JOptionPane.showMessageDialog(null, "Ett fel inträffade!");
-            }
-        
-        }
-      
+     
     }//GEN-LAST:event_buttonLoginAgentActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(StartWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new StartWindow().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField IdFieldAgent;
